c7a9e2fb1bf60d659ca75867c60dec497e8ef0c4
chboudry <chboudry@outlook.com>
2023-08-21 15:38:11 +0200

    shuffle added

--- generate_dataset_file.py
+++ generate_dataset_file.py
@@ -7,6 +7,7 @@ import math
 
 from generate_player_global_file import *
 from generate_training_dataset import *
+from shuffle_training_dataset import *
 
 ################################################################################
 ########################## Building dataset ####################################
@@ -19,3 +20,9 @@ from generate_training_dataset import *
 
 ## building of the training dataset
 generate_training_dataset("./data/atp_data.csv", "./data/players.csv", 2021, 2023, "./data/training_dataset.csv")
+
+
+## shuffle
+## Right now, player 1 is always the winner, we need to shuffle the data a bit to avoid that
+## Shuffle algorithm is we pick 5 row random out of 10 and make player 2 the winner (= switching player 1 and player 2 info and resetting player_winner1 accordingly)
+shuffle_training_dataset("./data/training_dataset.csv", "./data/training_dataset.csv")
---
+++ shuffle_training_dataset.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import random
+
+def shuffle_training_dataset(data_in, data_out):
+    # loading data in memory
+    data=pd.read_csv(data_in)
+    rowindex = random.sample(range(0, data.shape[0]-1), ((data.shape[0]-1)//2))
+
+    cols=[]
+
+    for col in data.columns:
+        if (col.startswith("player1_")):
+            cols.append(col[8:])
+
+    for index in rowindex:
+        for col in cols :
+            player1_col = "player1_" + col
+            player2_col = "player2_" + col
+            temp = data.iloc[index][player1_col]
+            data.at[data.index[index],player1_col] = data.at[data.index[index],player2_col]
+            data.at[data.index[index],player2_col] = temp
+        data.at[data.index[index],"winner_player1"] = 0
+
+    data.to_csv(data_out,index=False)
