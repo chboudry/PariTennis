5ef2f52e3c7fb2bc00e9597de89f7338f6197725
chboudry <chboudry@outlook.com>
2023-08-22 10:53:40 +0200

    m2c with new features

--- generate_training_dataset.py
+++ generate_training_dataset.py
@@ -13,22 +13,30 @@ def generate_training_dataset(atp_data_file, player_file, year_start, year_end, 
     # filtering the data raw to the wanted years
     matches = matches.loc[(matches.Date.dt.year >= year_start) & (matches.Date.dt.year <= year_end)]
 
-
     player1_name=[]
     player1_ATPRank=[]
     player1_games=[]
     player1_wins=[]
     player1_loses=[]
+    player1_indoors_wins=[]
+    player1_indoors_loses=[]
+    player1_outdoors_wins=[]
+    player1_outdoors_loses=[]
 
     player2_name=[]
     player2_ATPRank=[]
     player2_games=[]
     player2_wins=[]
     player2_loses=[]
+    player2_indoors_wins=[]
+    player2_indoors_loses=[]
+    player2_outdoors_wins=[]
+    player2_outdoors_loses=[]
 
     match_date=[]
     match_location=[]
     match_tournament=[]
+    match_court=[]
     match_surface=[]
     match_competition=[]
     match_odd_player1=[]
@@ -45,12 +53,20 @@ def generate_training_dataset(atp_data_file, player_file, year_start, year_end, 
             player1_games.append(player1.iloc[0]["games"])
             player1_wins.append(player1.iloc[0]["wins"])
             player1_loses.append(player1.iloc[0]["loses"])
+            player1_indoors_wins.append(player1.iloc[0]["indoors_win"])
+            player1_indoors_loses.append(player1.iloc[0]["indoors_loss"])
+            player1_outdoors_wins.append(player1.iloc[0]["outdoors_win"])
+            player1_outdoors_loses.append(player1.iloc[0]["outdoors_loss"])
         else:
             player1_name.append(row.Winner)
             player1_ATPRank.append(math.nan)
             player1_games.append(math.nan)
             player1_wins.append(math.nan)
             player1_loses.append(math.nan)
+            player1_indoors_wins.append(math.nan)
+            player1_indoors_loses.append(math.nan)
+            player1_outdoors_wins.append(math.nan)
+            player1_outdoors_loses.append(math.nan)
 
         if player2.size!=0:
             player2_name.append(player2.iloc[0]["name"])
@@ -58,15 +74,24 @@ def generate_training_dataset(atp_data_file, player_file, year_start, year_end, 
             player2_games.append(player2.iloc[0]["games"])
             player2_wins.append(player2.iloc[0]["wins"])
             player2_loses.append(player2.iloc[0]["loses"])
+            player2_indoors_wins.append(player2.iloc[0]["indoors_win"])
+            player2_indoors_loses.append(player2.iloc[0]["indoors_loss"])
+            player2_outdoors_wins.append(player2.iloc[0]["outdoors_win"])
+            player2_outdoors_loses.append(player2.iloc[0]["outdoors_loss"])
         else:
             player2_name.append(row.Loser)
             player2_ATPRank.append(math.nan)
             player2_games.append(math.nan)
             player2_wins.append(math.nan)
             player2_loses.append(math.nan)
+            player2_indoors_wins.append(math.nan)
+            player2_indoors_loses.append(math.nan)
+            player2_outdoors_wins.append(math.nan)
+            player2_outdoors_loses.append(math.nan)
 
         match_date.append(row.Date)
         match_surface.append(row.Surface)
+        match_court.append(row.Court)
         match_location.append(row.Location)
         match_tournament.append(row.Tournament)
         match_odd_player1.append(row.B365W)
@@ -79,16 +104,25 @@ def generate_training_dataset(atp_data_file, player_file, year_start, year_end, 
         'player1_games':player1_games,
         'player1_wins':player1_wins,
         'player1_loses':player1_loses,
+        'player1_indoors_wins':player1_indoors_wins,
+        'player1_indoors_loses':player1_indoors_loses,
+        'player1_outdoors_wins':player1_outdoors_wins,
+        'player1_outdoors_loses':player1_outdoors_loses,
 
         'player2_name' : player2_name,
         'player2_atprank':player2_ATPRank,
         'player2_games':player2_games,
         'player2_wins':player2_wins,
         'player2_loses':player2_loses,
+        'player2_indoors_wins':player2_indoors_wins,
+        'player2_indoors_loses':player2_indoors_loses,
+        'player2_outdoors_wins':player2_outdoors_wins,
+        'player2_outdoors_loses':player2_outdoors_loses,
 
         'match_date': match_date,
         'match_location':match_location,
         'match_tournament':match_tournament,
+        'match_court':match_court,
         'match_surface': match_surface,
         'match_odd_player1':match_odd_player1,
         'match_odd_player2':match_odd_player2,
