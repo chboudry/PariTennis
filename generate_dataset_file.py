ab6a47a252d1a3cda1deb3a9ceaa462a682a894c
chboudry <chboudry@outlook.com>
2023-08-22 09:49:39 +0200

    improved feature selection resilience in models

--- generate_dataset_file.py
+++ generate_dataset_file.py
@@ -16,7 +16,7 @@ from shuffle_training_dataset import *
 # generating players base using 2004 to 2020 data
 # This will be static data whatever the match studied
 # Execute only once each time you change the date parameters
-# generate_player_global_file("./data/atp_data.csv", 2004, 2020, "./data/players.csv")
+generate_player_global_file("./data/atp_data.csv", 2004, 2023, "./data/players.csv")
 
 ## building of the training dataset
 generate_training_dataset("./data/atp_data.csv", "./data/players.csv", 2021, 2023, "./data/training_dataset.csv")
