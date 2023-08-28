9ee559645a998df038e656238ff6f4516b32d027
chboudry <chboudry@outlook.com>
2023-08-28 12:17:59 +0200

    reordered dataset

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ from match_stats import *
 # Warning : this takes extensive time, up to 24h for match stats files from 2000 to 2023
 #generate_tournaments(2000, 2023, "./data/tournaments.csv")
 #generate_matchs(2000, 2023, "./data/matchs.csv")
-generate_match_stats("./data/matchs.csv","./data/matchs-stats.csv", "./data/matchs-stats-advanced.csv")
+generate_match_stats("./data/2023-matchs.csv","./data/2023-matchs-stats.csv", "./data/2023-matchs-stats-advanced.csv")
 #generate_players_overview("./data/matchs.csv", "./data/players.csv")
